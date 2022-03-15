8612541636b5c6e8ca55081c6f1a1aa1442aed28
Thomas Basham <Bashamtg@gmail.com>
2022-03-15 14:26:01 -0700

    updated img size

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,10 +4,11 @@ class HornedBeast extends React.Component {
   render() {
     return (
       <>
+        <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.src} alt={this.props.alt} title={this.props.imgTitle}>{this.props.image}</img>
+        <img src={this.props.src} alt={this.props.alt} title={this.props.imgTitle} width='200' height='200'>{this.props.image}</img>
         <p>{this.props.description}</p>
-
+        </article>
       </>
       
       
