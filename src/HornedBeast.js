8f5fb2eebcf1697efd0bb331185e64b7c211ba01
Thomas Basham <Bashamtg@gmail.com>
2022-03-15 15:35:32 -0700

    retrieved data and rendered images

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
 class HornedBeast extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      favorites: 0,
+    }
+  }
+handleFavorites = () => {
+  this.setState({
+    favorites: this.state.favorites + 1
+  });
+}
   render() {
     return (
       <>
         <article>
         <h2>{this.props.title}</h2>
-        <img src={this.props.src} alt={this.props.alt} title={this.props.imgTitle} width='200' height='200'>{this.props.image}</img>
+        <img 
+        onClick={this.state.favorites}// need to add state and favorti
+        src={this.props.image_url} 
+        alt={this.props.alt} 
+        title={this.props.imgTitle} 
+        width='200' 
+        height='200'
+        >{this.props.image}</img>
         <p>{this.props.description}</p>
         </article>
       </>
--- src/Main.js
+++ src/Main.js
@@ -3,18 +3,25 @@ import HornedBeast from './HornedBeast';
 // import billyGoat from './public/billy-goat.jpeg';
 
 class Main extends React.Component {
+
   render() {
     let hornedBeasts = [];
-    this.props.data.forEach(hornedBeast => {
+    this.props.data.forEach((hornedBeast) => {
       hornedBeasts.push(
+        <HornedBeast
+        image_url={hornedBeast.image_url}
         title={hornedBeast.title}
-        imageURL={hornedBeast.title}
+        description={hornedBeast.description}
+        // keyword={hornedBeast.keyword}
+        // horns={hornedBeast.horns}
+        />
       )
     })
+
     return (
       <main>
         {hornedBeasts}
-      {/* <HornedBeast title='Billy Goat' img src="billy-goat.jpeg" alt='Billy Goat' imgTitle='billy goat' description= 'A nimble and fierce horned beast'/> */}
+      {/* <HornedBeast title='Billy Goat' img imageURL="billy-goat.jpeg" alt='Billy Goat' imgTitle='billy goat' description= 'A nimble and fierce horned beast'/> */}
        </main>
 
     )
